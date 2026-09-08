5753c9bfe65a6066c0fde5cbfe4d50d496c47735
Jonathan Sharman <jonathan.sharman@niche.com>
2026-09-08 17:24:40 -0400

    Use atomic.Int32 instead of atomic.AddInt32

--- examples/generate/generic_mock.go
+++ examples/generate/generic_mock.go
@@ -12,9 +12,9 @@ import (
 type GenericMock[T interface{ byte | internal.Internal }, U any] struct {
 	T          *testing.T
 	GetTStub   func() T
-	GetTCalled int32
+	GetTCalled atomic.Int32
 	GetUStub   func() U
-	GetUCalled int32
+	GetUCalled atomic.Int32
 }
 
 // Verify that *GenericMock implements Generic.
@@ -25,7 +25,7 @@ func _[T interface{ byte | internal.Internal }, U any]() {
 // GetT is a stub for the Generic.GetT
 // method that records the number of times it has been called.
 func (m *GenericMock[T, U]) GetT() T {
-	atomic.AddInt32(&m.GetTCalled, 1)
+	m.GetTCalled.Add(1)
 	if m.GetTStub == nil {
 		if m.T != nil {
 			m.T.Error("GetTStub is nil")
@@ -38,7 +38,7 @@ func (m *GenericMock[T, U]) GetT() T {
 // GetU is a stub for the Generic.GetU
 // method that records the number of times it has been called.
 func (m *GenericMock[T, U]) GetU() U {
-	atomic.AddInt32(&m.GetUCalled, 1)
+	m.GetUCalled.Add(1)
 	if m.GetUStub == nil {
 		if m.T != nil {
 			m.T.Error("GetUStub is nil")
